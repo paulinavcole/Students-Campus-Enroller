ef984e4ae9b934216dac0d7ed19b04d36978dca9
Paulina Cole <lina.cole@gmail.com>
2022-07-23 16:00:04 -0400

    FIXED TIER TWO- complete

--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Campuses = ({ campuses, students }) => {
-    let enrollments;
+const Campuses = ({ students, campuses }) => {
     return (
-        <div>
-        <h2>Campuses</h2>
+        <ul>
             {campuses.map((campus) => {
                 return (
-                    <div>
                     <li key={campus.id}>
                         <Link to={`/campuses/${campus.id}`}>{campus.name}</Link>
                         <br></br>
@@ -19,24 +16,12 @@ const Campuses = ({ campuses, students }) => {
                         <br></br>
                         <img src ={campus.imageURL} width='400' height='300'/>
                         <br></br>
-                        {`(${(enrollments = students.filter((student) => student.campusId === campus.id)).length} ${enrollments.length === 1 ? 'enrollment' : 'enrollments'})`}
-                        <br></br>
+                        {campus.studentId === null ? `${campus.name} had no students.`: 'Students: '} 
+                        {students.map((student) => student.campusId === campus.id ? (<Link key={student.firstName} to={`/students/${student.id}`}> <p>{student.firstName}</p></Link>) : (' '))}
                     </li>
-                    <h2>Students</h2>
-                        {students.map((student) => {
-                         if (student.campusId === campus.id) {
-                            return (
-                                <ul>
-                                    <li key={campus.id}>
-                                    (<Link key={student.firstName} to={`/student/${student.id}`}> {student.firstName}</Link>)
-                                    </li>
-                                </ul>
-                            )
-                        }})}
-                    </div>
                 );
             })}
-        </div>
+        </ul>
     );
 };
 
