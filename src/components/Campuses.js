cc7adc55ef25f57cfb04278f5dd538f39aa5f980
Paulina Cole <lina.cole@gmail.com>
2022-07-24 21:47:37 -0400

    updated campus view to show enrollments per rubric

--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -5,7 +5,8 @@ import CampusForm from './CampusForm';
 import { deleteCampus } from '../store';
 
 
-const Campuses = ({ campuses, deleteCampus }) => {
+const Campuses = ({ campuses, deleteCampus, students }) => {
+    let enrollments;
 
     const deleteUserClick = (campus) => {
         deleteCampus(campus.id);
@@ -27,6 +28,8 @@ const Campuses = ({ campuses, deleteCampus }) => {
                             <br></br>
                             <div id='classphotos'>
                             <img src ={campus.imageURL}/>
+                            <br></br>
+                            <strong>Enrollments: </strong>{`(${(enrollments = students.filter((student) => student.campusId === campus.id)).length} ${enrollments.length === 1 ? 'enrollment' : 'enrollments'})`}
                             </div>
                         </li>
                     );
