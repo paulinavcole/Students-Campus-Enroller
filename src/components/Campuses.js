8c43f2413aa2dd69f1283650e19858ee2e8fb53a
Paulina Cole <lina.cole@gmail.com>
2022-07-27 18:57:09 -0400

    added styling to campus page with MUI

--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -3,53 +3,54 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import CampusForm from './CampusForm';
 import { deleteCampus } from '../store';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
+const Campuses = ({ campuses, students, deleteCampus }) => {
+  let enrollments;
 
-const Campuses = ({ campuses, deleteCampus, students }) => {
-    let enrollments;
+  const handleClick = (campus) => {
+    deleteCampus(campus.id);
+  };
 
-    const deleteUserClick = (campus) => {
-        deleteCampus(campus.id);
-    };
-
-    return (
-        <div id='campuses'>
-            <div id='campuslist'>
-            <ul>
-                {campuses.map((campus) => {
-                    return (
-                        <li key={campus.id}>
-                            <Link to={`/campuses/${campus.id}`}>{campus.name}</Link>
-                            <button onClick={() => deleteUserClick(campus)}>X</button>
-                            <br></br>
-                            <strong>Description: </strong>{campus.description}
-                            <br></br>
-                            <strong>Address: </strong> {campus.address}
-                            <br></br>
-                            <div id='classphotos'>
-                            <img src ={campus.imageURL}/>
-                            <br></br>
-                            <strong>Enrollments: </strong>{`(${(enrollments = students.filter((student) => student.campusId === campus.id)).length} ${enrollments.length === 1 ? 'enrollment' : 'enrollments'})`}
-                            </div>
-                        </li>
-                    );
-                })}
-            </ul>
-            </div>
-            <div id='campusform'>
-            <h2>Create New Campus: </h2>
-            <CampusForm />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="page-title">
+        {campuses.length === 0 ? 'Create A Campus' : 'Campuses'}
+      </h1>
+      <div className="campuses">
+        <ul>
+          {campuses.map((campus) => {
+            return (
+              <li key={campus.id} className="campus">
+                <HighlightOffIcon
+                  fontSize="small"
+                  onClick={() => handleClick(campus)}
+                />
+                <Link to={`/campuses/${campus.id}`}>{campus.name}</Link>
+                {`(${
+                  (enrollments = students.filter(
+                    (student) => student.campusId === campus.id
+                  )).length
+                }
+                ${enrollments.length === 1 ? 'enrollment' : 'enrollments'})`}
+                <div>Address: {campus.address}</div>
+                <img src ={campus.imageURL}/>
+              </li>
+            );
+          })}
+        </ul>
+        <CampusForm />
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => state;
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      deleteCampus: (id) => dispatch(deleteCampus(id)),
-    };
+  return {
+    deleteCampus: (id) => dispatch(deleteCampus(id)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Campuses);
