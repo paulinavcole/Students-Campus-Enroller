80bf6a036f6f62894d96b9106fde85d09b24ca4c
Paulina Cole <lina.cole@gmail.com>
2022-07-27 19:01:24 -0400

    added mui styling to studens page

--- src/components/Students.js
+++ src/components/Students.js
@@ -3,48 +3,62 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import StudentForm from './StudentForm';
 import { deleteStudent } from '../store';
-
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 const Students = ({ students, campuses, deleteStudent }) => {
+  return (
+    <div>
+      <h1 className="page-title">
+        {students.length === 0 ? 'Create A Student' : 'Students'}
+      </h1>
+      <div className="students">
+        <ul>
+          {students.map((student) => {
+            return (
+              <li key={student.id} className="student">
+                <HighlightOffIcon
+                  fontSize="small"
+                  onClick={() => deleteStudent(student.id)}
+                  className="delete-button"
+                />
 
-    const deleteStudentClick = (student) => {
-        deleteStudent(student.id);
-    };
+                <Link to={`/students/${student.id}`}>
+                  {student.firstName} {student.lastName}
+                </Link>
+                <div>
+                  Email: <a href={`mailto:${student.email}`}>{student.email}</a>{' '}
+                </div>
+                <div>GPA: {student.gpa}</div>
+                <img src ={student.imageURL}/>
+                {student.campusId === null
+                  ? `${student.firstName} does not attend any campus.`
+                  : 'Campus: '}
+                {campuses.map((campus) =>
+                  student.campusId === campus.id ? (
+                    <Link key={campus.name} to={`/campuses/${campus.id}`}>
+                      {campus.name}
+                    </Link>
 
-    return (
-        <div id='studentinfo'>
-            <ul>
-                {students.map((student) => {
-                    return (
-                        <li key={student.id}>
-                            <Link to={`/students/${student.id}`}>{student.firstName} {student.lastName}</Link>
-                            <button onClick={() => deleteStudentClick(student)}>X</button>
-                            <br></br>
-                            Email: {student.email}
-                            <br></br>
-                            GPA: {student.gpa}
-                            <br></br>
-                            <img src ={student.imageURL} width='100' height='200'/>
-                            <br></br>
-                            {student.campusId === null ? `${student.firstName} does not attend any campus.`: 'Campus: '} {campuses.map((campus) => student.campusId === campus.id ? (<Link key={campus.name} to={`/campuses/${campus.id}`}> {campus.name}</Link>) : (''))}
-                        </li>
-                    );
-                })}
-            </ul>
-            <div id='studentform'>
-            <h2>Create New Student: </h2>
-            <StudentForm />
-            </div>
-        </div>
-    );
+                  ) : (
+                    ''
+                  )
+                )}
+              </li>
+            );
+          })}
+        </ul>
+        <StudentForm />
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => state;
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      deleteStudent: (id) => dispatch(deleteStudent(id)),
-    };
+  return {
+    deleteStudent: (id) => dispatch(deleteStudent(id)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Students);
