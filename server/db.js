a11f0e284557087a7e89e0ea2ac9edb01f2e814e
Paulina Cole <lina.cole@gmail.com>
2022-07-28 22:40:06 -0400

    added db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_schools_db');
+
 const { STRING, TEXT, DECIMAL } = Sequelize;
 
 const Student = conn.define('student', {
