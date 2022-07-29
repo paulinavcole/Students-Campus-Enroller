dfc940d15d9488dcebd4013f05424e521bd9e4de
Paulina Cole <lina.cole@gmail.com>
2022-07-28 23:05:59 -0400

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div id='homepage'>
         <h1>Welcome To Our Campus Enrollments</h1>
         <img src='https://i.pinimg.com/originals/be/6f/ed/be6fed3444cd73a8c2ba5f55aa9e979d.jpg' />
         </div>
